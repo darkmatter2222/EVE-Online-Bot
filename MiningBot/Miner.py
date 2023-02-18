e1e6b6ded912b41bbe3577606df130f7a5a5acd9
ryan susman <ryansusman@gmail.com>
2023-02-17 21:17:21 -0500

    checkpoint

--- MiningBot/Miner.py
+++ MiningBot/Miner.py
@@ -1,5 +1,5 @@
 from MiningBot.EveInterface.Interface import Interface
-import json
+import json, time
 import pyautogui
 
 config_dir = r'Configs/configs.json'
@@ -17,8 +17,23 @@ resource_map = {}
 
 
 def perform_mapping():
+    global resource_map
+    targets = ['Site1 P1', 'Site2 P1', 'Site3 P1']
     df = game.get_location_data(refresh_screen=True)
 
+    for target in targets:
+        xy = df.loc[df['Name'] == target, 'click_target'].values[0]
+        pyautogui.moveTo(xy)
+        time.sleep(0.1)
+        pyautogui.click(button='right')
+        time.sleep(0.1)
+        pyautogui.moveTo(xy[0] + 50, xy[1] + 25)
+        time.sleep(0.1)
+        pyautogui.click(button='left')
+        time.sleep(0.1)
+        pyautogui.moveTo(1, 1)
+
+    time.sleep(60)
 
 # warp to playground and begin mapping
 print(perform_mapping())
