1a265863a640b0f830df3f649b4a83108de4d65a
ryan susman <ryansusman@gmail.com>
2023-02-17 20:47:27 -0500

    checkpoint

--- MinningBot/EveInterface/Interface.py
+++ MinningBot/EveInterface/Interface.py
@@ -1,9 +1,11 @@
 import numpy as np
+import pandas as pd
 import mss
 import mss.tools
 from PIL import Image, ImageDraw
 import cv2
 import sys, os, decimal, json
+from pytesseract import pytesseract
 
 sys.path.append(os.path.realpath('..'))
 
@@ -29,11 +31,62 @@ def get_row_points(box, num_of_scanned_rows):
     return final_row_points
 
 
+def get_col_points(box, col_coefs):
+    col_delta = box[2] - box[0]
+    cols_points = []
+    for col_coef in col_coefs:
+        cols_points.append(col_delta * col_coef)
+
+    temp_list = []
+    for i in range(len(cols_points)):
+        this_int = box[0]
+        for j in range(i + 1):
+            this_int += cols_points[j]
+        temp_list.append(this_int)
+
+    final_col_points = np.array(temp_list)
+    final_col_points = list(np.rint(final_col_points).astype(int))
+    final_col_points = [box[0]] + final_col_points + [box[2]]
+    return final_col_points
+
+
+def get_cells(x_range, y_range):
+    cells = {}
+    for i, x in enumerate(x_range):
+        for j, y in enumerate(y_range):
+            try:
+                cells[i, j] = tuple([x, y, x + x_range[i + 1], y + y_range[j + 1]])
+            except:  # lazy
+                pass
+    return cells
+
+
+def extract_values(img, cells, x_range, y_range, columns, monitor_x_offset, monitor_y_offset):
+    frames = []
+    for i in range(len(y_range) - 1):
+        frame = {}
+        for j in range(0, len(x_range) - 1):
+            cell = cell_dims_from_list(list(cells[j, i]))
+            cell_image = img.crop(cell)
+
+            transcript = pytesseract.image_to_string(cell_image, lang='eng')
+
+            frame[columns[j]] = transcript.replace('\n', '').replace('+', '').replace('>', '').replace('k m',
+                                                                                                       ' km').replace(
+                ',', '')
+        frame['click_target'] = (
+            cells[0, i][0] + 60 + monitor_x_offset, cells[0, i][1] + 15 + monitor_y_offset)  # offset by 10x10 pixels
+        frames.append(frame)
+    df = pd.DataFrame(frames, columns=columns)
+    return df
+
+
 class Interface:
     def __init__(self,
                  config_dir=r'..\Configs\configs.json'):
         self.config_dir = config_dir
         self.config = json.load(open(self.config_dir))
+        self.screen = self.get_screen()
 
     def get_screen(self):
         with mss.mss() as sct:
@@ -54,10 +107,38 @@ class Interface:
             img = Image.fromarray(img)
             return img
 
-    def get_survey_scan_data(self, rows=6):
+    def get_survey_scan_data(self, rows=6, refresh_screen=False):
         y_range = get_row_points(self.config['survey_scan_box'], rows)
+        x_range = get_col_points(self.config['survey_scan_box'], self.config['survey_scan_box_columns'])
+        cells = get_cells(x_range, y_range)
+
+        if refresh_screen:
+            self.screen = self.get_screen()
+
+        extract_columns = ['Locked', 'Ore', 'Quantity', 'Volume', 'Distance', 'click_target']
+
+        return extract_values(img=self.screen, cells=cells, x_range=x_range,
+                              y_range=y_range, columns=extract_columns,
+                              monitor_x_offset=self.config['monitor_offset_x'],
+                              monitor_y_offset=self.config['monitor_offset_y'])
+
+    def get_location_data(self, rows=13, refresh_screen=False):
+        y_range = get_row_points(self.config['locations_box'], rows)
+        x_range = get_col_points(self.config['locations_box'], self.config['locations_box_columns'])
+        cells = get_cells(x_range[0:2], y_range)
+
+        if refresh_screen:
+            self.screen = self.get_screen()
+
+        extract_columns = ['Name', 'click_target']
+
+        return extract_values(img=self.screen, cells=cells, x_range=x_range[0:2],
+                              y_range=y_range, columns=extract_columns,
+                              monitor_x_offset=self.config['monitor_offset_x'],
+                              monitor_y_offset=self.config['monitor_offset_y'])
 
 
 I = Interface()
 
-I.get_screen().show()
+df = I.get_location_data()
+print(df)
