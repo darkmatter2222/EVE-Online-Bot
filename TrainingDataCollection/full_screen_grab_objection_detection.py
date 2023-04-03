8ff990c5fdab26412debe5c9fee5df9beb962035
ryan susman <ryansusman@gmail.com>
2023-04-02 21:39:30 -0400

    checkpoint

--- TrainingDataCollection/full_screen_grab_objection_detection.py
+++ TrainingDataCollection/full_screen_grab_objection_detection.py
@@ -1,5 +1,6 @@
 import sys, os, decimal, json
 import socket, uuid, time
+from PIL import Image
 
 sys.path.append(os.path.realpath('..'))
 from MiningBot.EveInterface.Interface import Interface, get_cells, get_row_points, get_col_points
@@ -9,13 +10,14 @@ config_dir = r'../MiningBot/Configs/configs.json'
 config = json.load(open(config_dir))[socket.gethostname()]
 game = Interface(config_dir=config_dir)
 
-data_root = r'O:\source\repos\data_labeler\training_data\inventory'
+data_root = r'O:\eve_models\training_data\unclass'
 
 logger.add(config['log_dir'] + '\\' + socket.gethostname() + "_" + sys.argv[0].split('/')[-1:][0] + "_{time}.log")
 
 while True:
     img = game.get_screen()
     id = uuid.uuid1()
+    img = img.crop((0, 0, 500, 600))
     img.save(f"{data_root}\\{id}.png")
     logger.info(f'Saved image:{id}')
-    time.sleep(0.1)
+    time.sleep(1)
