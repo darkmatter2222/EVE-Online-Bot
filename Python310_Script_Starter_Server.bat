661b0da70fe2a9a901d7a004914e893f143bed7a
ryan susman <ryansusman@gmail.com>
2023-04-30 19:41:41 -0400

    startup

--- Python310_Script_Starter_Server.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-start /min cmd /k "C: & cd C:\Users\ryans\source\repos\EVE-Online-Bot"
-start /min cmd /k "C: & cd C:\Users\ryans\source\repos\venvs\Python310\Scripts & activate & cd /d C:\Users\ryans\source\repos\EVE-Online-Bot\AI_Pilot & python -m StartMining.py"
