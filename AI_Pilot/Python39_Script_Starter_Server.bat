2340de7bcd4dca7665a2cc1600ab57b5e835a6e3
ryan susman <ryansusman@gmail.com>
2023-04-30 20:05:02 -0400

    startup

--- AI_Pilot/Python39_Script_Starter_Server.bat
+++ AI_Pilot/Python39_Script_Starter_Server.bat
@@ -1,3 +1,3 @@
 @echo off
 start /min cmd /k "C: & cd C:\Users\ryans\source\repos\EVE-Online-Bot"
-start /min cmd /k "C: & cd C:\Users\ryans\source\repos\venvs\Python310\Scripts & activate & cd /d C:\Users\ryans\source\repos\EVE-Online-Bot & python -m AI_pilot\Start_AI_Pilot.py --config_file AI_Pilot\ai_pilot_config_v2.json --headless_miner 1"
+start /min cmd /k "C: & cd C:\Users\ryans\source\repos\venvs\Python310\Scripts & activate & cd /d C:\Users\ryans\source\repos\EVE-Online-Bot & python -m AI_Pilot\Start_AI_Pilot.py --config_file AI_Pilot\ai_pilot_config_v2.json --headless_miner 1"
