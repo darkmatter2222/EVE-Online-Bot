951b49e1538de7a4ef60468da33ee384589f44a1
ryan susman <ryansusman@gmail.com>
2023-04-30 20:12:15 -0400

    startup

--- Archive/Python310_Jupyter_Lab_Starter.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-start /min cmd /k "O: & cd O:\source\repos\EVE-Online-Bot"
-start /min cmd /k "O: & cd O:\source\repos\venv\Python310\Scripts & activate & cd O:\source\repos\EVE-Online-Bot"
-start /min cmd /k "O: & cd O:\source\repos\venv\Python310\Scripts & activate & cd /d O:\source\repos\EVE-Online-Bot & python -m jupyter lab"
