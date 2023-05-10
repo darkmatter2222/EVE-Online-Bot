45e35fc71ee24d2ca2aea9fcc1a4182af9115ddf
ryan susman <ryansusman@gmail.com>
2023-05-09 21:21:31 -0400

    move to jita

--- AI_Pilot/Objectives/Aggregate_Resources/Aggregate_Resources.py
+++ AI_Pilot/Objectives/Aggregate_Resources/Aggregate_Resources.py
@@ -4,6 +4,8 @@ from AI_Pilot.Game_Functions.Common.Common import exit_hanger
 from AI_Pilot.Game_Functions.Navigation.Assets_Navigation import get_assets_search_table, get_set_dest_options
 from AI_Pilot.Game_Functions.Navigation.Waypoint_Navigation import navigate_waypoints_to_end
 from AI_Pilot.Control_Functions.Monitors import get_screen
+from AI_Pilot.Control_Functions.General import get_cords_with_offset
+import pyautogui
 from loguru import logger
 from PIL import Image
 import time
@@ -18,32 +20,45 @@ def Aggregate_Resources(ag):
 
 def unload_at_jita(ag):
     # =============================================================
-    df = get_assets_search_table(ag)
-    logger.info(df)
-    df = df[df['Name'].str.contains('Jita')].reset_index(drop=True)
-    logger.info(df)
-    perform_move_click(ag, df.loc[0, 'click_target'], button='right', perform_offset=False)
+    perform_move_click(ag, ag.static_screen_pos['click_target_first_item_in_inventory'],
+                       button='right', perform_offset=True)
+    time.sleep(1)
+    perform_move_click(ag, (400, 1000), button='left',
+                       perform_offset=True)
+    time.sleep(1)
+
+    jita_name = 'Jita IV - Moon 4 - Caldari Navy Assembly Plant'
+
+    perform_move_click(ag, pos=(156, 130), button='left', perform_offset=True)
+    pyautogui.write(jita_name)
+    pyautogui.press('enter')
+    time.sleep(3)
+
+    click_target = get_cords_with_offset(ag, 1500, 683)
+
+
+    # =============================================================
+    perform_move_click(ag, click_target, button='right', perform_offset=False)
 
     start_offset = [-10, 4]
     resolution = [256, 396]
 
-    crop_dims = (df.loc[0, 'click_target'][0] - ag.monitor_spec['monitor_offset'][0] - start_offset[0],
-                 df.loc[0, 'click_target'][1] - ag.monitor_spec['monitor_offset'][1] - start_offset[1],
-                 df.loc[0, 'click_target'][0] - ag.monitor_spec['monitor_offset'][0] - start_offset[0] + resolution[0],
-                 df.loc[0, 'click_target'][1] - ag.monitor_spec['monitor_offset'][1] - start_offset[1] + resolution[1])
+    crop_dims = (click_target[0] - ag.monitor_spec['monitor_offset'][0] - start_offset[0],
+                 click_target[1] - ag.monitor_spec['monitor_offset'][1] - start_offset[1],
+                 click_target[0] - ag.monitor_spec['monitor_offset'][0] - start_offset[0] + resolution[0],
+                 click_target[1] - ag.monitor_spec['monitor_offset'][1] - start_offset[1] + resolution[1])
 
     img = get_screen(ag)
     img = img.crop(crop_dims)
 
     set_dest_result = get_set_dest_options(ag, img)
 
-    click_target = (0, 0)
     if set_dest_result['class'] == 'second_pos':
-        click_target = (df.loc[0, 'click_target'][0] + 80, df.loc[0, 'click_target'][1] + 70)
+        click_target = (click_target[0] + 80, click_target[1] + 70)
     elif set_dest_result['class'] == 'third_pos':
-        click_target = (df.loc[0, 'click_target'][0] + 80, df.loc[0, 'click_target'][1] + 110)
+        click_target = (click_target[0] + 80, click_target[1] + 110)
     elif set_dest_result['class'] == 'seventh_pos':
-        click_target = (df.loc[0, 'click_target'][0] + 80, df.loc[0, 'click_target'][1] + 230)
+        click_target = (click_target[0] + 80, click_target[1] + 230)
 
     perform_move_click(ag, click_target, button='left', perform_offset=False)
     # =============================================================
